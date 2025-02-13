b3a57e6c5efbb13eec3cf7f920c292b095806d36
hectorHardy <hectorhardy02@gmail.com>
2025-02-13 20:51:12 +0000

    gradle build modified, apk changed from 33 to 34. app runs now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "uk.ac.rgu.ecodrive"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "uk.ac.rgu.ecodrive"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,7 +32,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")

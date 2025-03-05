ee4d59d0ffa4d094be35773aa3c38014f32c5eef
hectorHardy <hectorhardy02@gmail.com>
2025-03-05 23:01:59 +0000

    Firebase DB initialised. Login page layout complete.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,6 +36,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.navigation:navigation-runtime:2.8.7")
     implementation("androidx.navigation:navigation-fragment:2.8.7")
+    implementation("com.google.firebase:firebase-database:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")

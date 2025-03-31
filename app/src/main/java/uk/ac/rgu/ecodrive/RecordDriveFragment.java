e0c700e559b73fed0629521f843f95db5a454ea0
hectorHardy <hectorhardy02@gmail.com>
2025-03-31 19:52:37 +0100

    now multiple limits are compared to user speeds. bug stopping location request returning more than one location

--- app/src/main/java/uk/ac/rgu/ecodrive/RecordDriveFragment.java
+++ app/src/main/java/uk/ac/rgu/ecodrive/RecordDriveFragment.java
@@ -1,13 +1,19 @@
 package uk.ac.rgu.ecodrive;
 
+
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.Manifest;
+
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -16,6 +22,10 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+
+import android.os.Looper;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +34,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -31,8 +42,10 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 
+
 import java.util.ArrayList;
 import java.util.List;
+
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -42,6 +55,7 @@ import uk.ac.rgu.ecodrive.api.RetrofitClient;
 import uk.ac.rgu.ecodrive.models.OverpassResponse;
 import uk.ac.rgu.ecodrive.models.LocationData;
 
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link RecordDriveFragment#newInstance} factory method to
@@ -49,10 +63,12 @@ import uk.ac.rgu.ecodrive.models.LocationData;
  */
 public class RecordDriveFragment extends Fragment implements View.OnClickListener, SensorEventListener {
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     private boolean isRecording = false; // Flag to track button state
     private Button btn_record; // Declare button globally
@@ -65,13 +81,16 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
     private LocationCallback locationCallback;
     private List<LocationData> locationList = new ArrayList<>(); // Store recorded locations
 
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+
     public RecordDriveFragment() {
         // Required empty public constructor
     }
+
 
     /**
      * Use this factory method to create a new instance of
@@ -91,6 +110,7 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,20 +120,22 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
         }
     }
 
+
     private final ActivityResultLauncher<String> locationPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-//                    startLocationUpdates();
-                } else {
+                if (!isGranted) {
                     Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
                 }
             });
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+
 
         // Check if permission is granted
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
@@ -124,10 +146,13 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
             //startLocationUpdates();
         }
 
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_record_drive, container, false);
 
+
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
@@ -137,22 +162,28 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
         btn_record = view.findViewById(R.id.btn_record);
         btn_record.setOnClickListener(this);
 
+
         txt_accX = view.findViewById(R.id.txt_accX);
         txt_accY = view.findViewById(R.id.txt_accY);
         txt_accZ = view.findViewById(R.id.txt_accZ);
+
 
         sensorManager = (SensorManager) requireActivity().getSystemService(getContext().SENSOR_SERVICE);
         if (sensorManager != null) {
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         }
+
     }
+
 
     @Override
     public void onClick(View v) {
 
+
         if (v.getId() == R.id.btn_record) { // mix of chatGPT and appDev module code
             System.out.println("record button clicked");
             isRecording = !isRecording; // Toggle state
+
 
             if (isRecording) {
                 btn_record.setText(getString(R.string.btn_record_end));
@@ -165,11 +196,13 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
                 sensorManager.unregisterListener(this);
                 stopLocationUpdates();
                 Log.d("locationList", locationList.toString());
-                fetchSpeedLimits(locationList);
+                fetchSpeedLimits();
             }
+
 
         }
     }
+
 
     @Override
     public void onSensorChanged(SensorEvent event) {
@@ -178,29 +211,37 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
             float y = event.values[1];
             float z = event.values[2];
 
+
             double acceleration = Math.sqrt(x*x + y*y + z*z);
             txt_accX.setText(String.format("acceleration: %.2f m/s²", acceleration));
         }
     }
 
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+
     }
+
 
     private void startLocationUpdates() {
         // Create LocationRequest using LocationRequest.Builder with the new Priority enum
-        // Optional: Set the max wait time
-        // check correct import
-        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 2000)
-                .setMinUpdateIntervalMillis(1000)  // Optional: Set the max wait time
-                .build();
+        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_BALANCED_POWER_ACCURACY, 300).build();
+
+
+        Log.d("update", "check 1,2,1,2");
+
 
         // Set up a LocationCallback to handle location updates
         locationCallback = new LocationCallback() {
             @Override
-            public void onLocationResult(LocationResult locationResult) {
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                Log.d("LocationUpdates", "Location count: " + locationResult.getLocations().size());
+
+                Log.d("LocationUpdates", "Received location update!");
                 if (locationResult == null) {
+                    Log.d("LocationUpdates", "LocationResult is null");
                     return;
                 }
                 for (Location location : locationResult.getLocations()) {
@@ -210,12 +251,15 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
                         double latitude = location.getLatitude();
                         double longitude = location.getLongitude();
 
+
                         // Save location to list
                         locationList.add(new LocationData(latitude, longitude, speedKmh));
+
 
                         // Log & Display Location Data
                         Log.d("Location", "Lat: " + latitude + ", Lng: " + longitude);
                         Log.d("Speed", "Current Speed: " + speedKmh + " km/h");
+
 
                         // Update UI with current speed
                         txt_accY.setText("Speed: " + speedKmh + " km/h");
@@ -225,37 +269,49 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
             }
         };
 
+
         // Check if the app has permission before requesting location updates
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+                    .addOnFailureListener(e -> {
+                        Log.e("LocationUpdates", "Failed to request location updates: " + e.getMessage());
+                    });
+            Log.d("LocationUpdates", "Location updates requested");
+        } else {
+            // ... (permission request)
+            Log.d("LocationUpdates", "Location permission not granted");
         }
     }
 
-    private void fetchSpeedLimits(List<LocationData> locations) {
-        if (locations.isEmpty()) {
+
+
+
+    private void fetchSpeedLimits() {
+        if (locationList.isEmpty()) {
             Log.d("SpeedLimit", "No locations available to fetch speed limits.");
             return;
         }
 
+
         // Build the Overpass API Query for Multiple Locations
         StringBuilder query = new StringBuilder("[out:json];");
-        for (int i = 0; i < locations.size(); i++) {
-            // Only get every 5th location (i % 5 == 0)
-            if (i % 5 == 0) {
-                LocationData location = locations.get(i);
-                double lat = location.getLatitude();
-                double lon = location.getLongitude();
-                query.append("way(around:20,").append(lat).append(",").append(lon).append(")[maxspeed];");
-            }
+          for (int i = 0; i < locationList.size(); i=i+5 ) {
+            LocationData location = locationList.get(i);
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+            query.append("way(around:20,").append(lat).append(",").append(lon).append(")[maxspeed];");
         }
         query.append("out;"); // Complete the query
+
 
         OverpassApiService apiService = RetrofitClient.getClient();
         Call<OverpassResponse> call = apiService.getSpeedLimit(query.toString());
 
+
         // Log the full query URL before making the request
         Log.d("SpeedLimit", "Query: " + query.toString());
+
 
         call.enqueue(new Callback<OverpassResponse>() {
             @Override
@@ -264,13 +320,17 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
                 if (response.isSuccessful() && response.body() != null) {
                     List<OverpassResponse.Element> elements = response.body().elements;
                     if (elements != null && !elements.isEmpty()) {
+                        int z = 0;
                         for (int i = 0; i < elements.size(); i++) {
                             OverpassResponse.Element element = elements.get(i);
+                            float speed_temp = locationList.get(z).getSpeed();
                             if (element.tags != null && element.tags.maxspeed != null) {
                                 Log.d("SpeedLimit", "Location " + (i + 1) + ": " + element.tags.maxspeed + " km/h");
+                                Log.d("SPEED VIOLATION CHECK: ", "limit: " + element.tags.maxspeed + " km/h, actual speed: " + speed_temp);
                             } else {
                                 Log.d("SpeedLimit", "Location " + (i + 1) + ": No speed limit found");
                             }
+                            z = z+5;
                         }
                     } else {
                         Log.d("SpeedLimit", "No speed limit data found for any location");
@@ -280,6 +340,7 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
                 }
             }
 
+
             @Override
             public void onFailure(Call<OverpassResponse> call, Throwable t) {
                 Log.e("SpeedLimit", "API call failed: " + t.getMessage());
@@ -287,14 +348,18 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
         });
     }
 
+
     private void stopLocationUpdates() {
+        Log.d("SATOPPED", "stopped updates");
         if (fusedLocationClient != null) {
             fusedLocationClient.removeLocationUpdates(locationCallback);
         }
     }
 
+
     @Override
     public void onDestroyView() { //  stop listening for sensor input when fragment is left
+        Log.d("STOPPED", "stopped");
         super.onDestroyView();
         if (sensorManager != null) {
             sensorManager.unregisterListener(this);
@@ -303,5 +368,7 @@ public class RecordDriveFragment extends Fragment implements View.OnClickListene
         }
     }
 
+
 }
+
 
